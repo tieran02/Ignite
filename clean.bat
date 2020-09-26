eba3b08c2ca6eed537c328e33c0c3885f5fb1231
Tieran <tieran.wightman@gmail.com>
2020-09-26 10:03:25 +0100

    updated clean.bat

--- clean.bat
+++ clean.bat
@@ -1,7 +1,12 @@
-del /S *.sln
-del /S *.user
-del /S *.vcxproj
-del /S *.filters
+del /S Ignite-Core.vcxproj
+del /S Ignite-Core.vcxproj.filters
+del /S Ignite-Core.filters
+del /S Ignite-Sandbox.vcxproj
+del /S Ignite-Sandbox.vcxproj.filters
+del /S Ignite-Sandbox.vcxproj.user
+del /S Ignite-Sandbox.filters
+
 rd /s /q .vs
 rd /s /q bin-int
 rd /s /q bin
+del Ignite-Sandbox.sln
